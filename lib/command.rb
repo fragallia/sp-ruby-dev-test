5316ef3ee3c0d2936cf751d5ddcb788f47e11025
Arturs Kreipans <arturs.kreipans@gmail.com>
2022-08-21 20:12:10 +0300

    Adds page views output

--- lib/command.rb
+++ lib/command.rb
@@ -1,31 +1,75 @@
+# frozen_string_literal: true
+
 require 'optparse'
 
 class CommandError < StandardError; end
 
 class Command
-  attr_reader :args
+  attr_accessor :args, :options
+
   def initialize(args)
     @args = args
+    @options = {}
   end
 
   def run!
-    options = {}
-    opts = OptionParser.new do |opts|
-      opts.banner = "Usage: bin/parser.rb [options]"
+    option_parser = OptionParser.new do |opts|
+      opts.banner = 'Usage: bin/parser.rb [options]'
 
-      opts.on("-h", "--help", "Prints this help") do
+      opts.on('-f', '--file PATH_TO_FILE', 'Log file') do |v|
+        options[:file] = v
+      end
+      opts.on('-h', '--help', 'Prints this help') do
         puts opts
         exit
       end
     end
 
-    opts.parse(args)
+    option_parser.parse(args)
 
     if options.empty?
-      puts opts
+      puts option_parser
       exit
     end
+
+    puts 'List of webpages with most page views'
+    puts
+
+    page_views.each { |item| puts item.join(' ') }
+
+    puts
+    puts 'List of webpages with most unique page views'
+    puts
+
+    page_unique_views.each { |item| puts item.join(' ') }
   rescue OptionParser::InvalidOption => e
     raise CommandError, e.message
+  end
+
+  private
+
+  def page_unique_views
+    stats.map { |page, ips| [page, ips.values.max, 'unique views'] }
+         .sort_by { |item| item[1] }
+         .reverse
+  end
+
+  def page_views
+    stats.map { |page, ips| [page, ips.values.sum, 'visits'] }
+         .sort_by { |item| item[1] }
+         .reverse
+  end
+
+  def stats
+    @stats ||= logs.each_with_object({}) do |log, res|
+      res[log[:path]] ||= {}
+      res[log[:path]][log[:ip]] = res[log[:path]][log[:ip]].to_i + 1
+    end
+  end
+
+  def logs
+    @logs ||= File.readlines(options[:file]).map do |line|
+      line.strip.split.then { |chunks| { path: chunks[0], ip: chunks[1] } }\
+    end
   end
 end
--- spec/lib/command_spec.rb
+++ spec/lib/command_spec.rb
@@ -1,16 +1,40 @@
+# frozen_string_literal: true
+
 require_relative '../../lib/command'
 
 RSpec.describe(Command) do
   describe('.run!') do
     subject(:run!) { described_class.new(args).run! }
 
-    let(:args) { ['-f file_path'] }
+    let(:path) { 'spec/fixtures/simple.log' }
+    let(:args) { ['-f', path] }
+
+    context 'when log file provided' do
+      let(:expected_output) do
+        [
+          'List of webpages with most page views',
+          '',
+          '/contact 2 visits',
+          '/help_page/1 1 visits',
+          '',
+          'List of webpages with most unique page views',
+          '',
+          '/contact 2 unique views',
+          '/help_page/1 1 unique views',
+          ''
+        ].join("\n")
+      end
+
+      it 'outputs header for page views stats' do
+        expect { run! }.to output(expected_output).to_stdout
+      end
+    end
 
     context 'with wrong args' do
       let(:args) { ['--not-exists param'] }
 
       it 'outputs help' do
-        expect { run! }.to raise_error(CommandError, %r{invalid option: --not-exists param})
+        expect { run! }.to raise_error(CommandError, /invalid option: --not-exists param/)
       end
     end
 
